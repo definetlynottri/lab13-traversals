a754ef5de09ddc9efcd05fd22204419ade2ccaf7
Sean Sanghera <seansanghera3@gmail.com>
2022-05-06 12:16:58 -0700

    updated traversal code

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -156,7 +156,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//For a bst this will print the values in sorted order from smallest to largest
 	public void inOrder() {
 		inOrderRecurse(root); 
-		System.out.println("InOrder test commit");
+		System.out.println("Sean tests in order");
 	}
 	
 	public void inOrderRecurse(BSTNode<T> node) {
